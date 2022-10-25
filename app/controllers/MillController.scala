aa1982d3ec606dd0eeef6eb356b4ed093e5e4d69
Melanie Galip <melanie.galip@outlook.de>
2022-10-25 08:10:11 +0200

    feat: adjust route controller

--- app/controllers/MillController.scala
+++ app/controllers/MillController.scala
@@ -24,36 +24,28 @@ class MillController @Inject() (
   val injector: Injector = Guice.createInjector(new MillModule)
   val gameController = injector.getInstance(classOf[ControllerInterface])
   val tui = new TUI(gameController)
-  var gameMessage: String = ""
   gameController.add(this)
+
+  var gameStatus: String = ""
 
   override def update(message: Option[String], e: Event.Value) = {
     e match {
-      case Event.QUIT => gameMessage = "quit"
+      case Event.QUIT => gameStatus = "The game has been quitted."
       case Event.PLAY =>
-        if (message.isDefined) gameMessage = message.get
+        if (message.isDefined) gameStatus = message.get
         else
-          gameMessage =
-            s"${gameController.gameState.get.game.currentPlayer}'s turn(${gameController.currentGameState}): " + gameController.gameState.get.game.board
+          gameStatus =
+            s"${gameController.gameState.get.game.currentPlayer}'s turn(${gameController.currentGameState})."
     }
-  }
-
-  /** Create an Action to render an HTML page.
-    *
-    * The configuration in the `routes` file means that this method will be
-    * called when the application receives a `GET` request with a path of `/`.
-    */
-  def about() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
   }
 
   def index = Action { implicit request: Request[AnyContent] =>
     if (!gameController.hasFirstPlayer)
-      Ok(Messages.introductionText)
+      Ok(views.html.index(Messages.introductionText))
     else if (!gameController.hasSecondPlayer)
-      Ok(Messages.addSecondPlayerText)
+      Ok(views.html.index(Messages.addSecondPlayerText))
     else
-      Ok(gameMessage)
+      Ok(views.html.mill(gameStatus, gameController.gameState.get.game.board))
   }
 
   def command(cmd: String) = Action { implicit request: Request[AnyContent] =>
@@ -63,13 +55,13 @@ class MillController @Inject() (
   private def onCommand(cmd: String): Result = {
     if (!gameController.hasFirstPlayer && !cmd.isBlank) {
       gameController.addFirstPlayer(cmd)
-      return Ok(Messages.addSecondPlayerText)
+      return Ok(views.html.index(Messages.addSecondPlayerText))
     } else if (!gameController.hasSecondPlayer && !cmd.isBlank) {
       gameController.addSecondPlayer(cmd)
       gameController.newGame
     } else if (!cmd.isBlank) {
       tui.onInput(cmd)
     }
-    Ok(gameMessage)
+    Ok(views.html.mill(gameStatus, gameController.gameState.get.game.board))
   }
 }
