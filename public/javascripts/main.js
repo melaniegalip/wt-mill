e29c79526f94f7a618380590eccdbd5e01503d80
Melanie Galip <melanie.galip@outlook.de>
2022-11-07 17:03:44 +0100

    feat: basic game functionality

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,12 @@
 import '/assets/lib/bootstrap/js/bootstrap.min.js';
+
+import { gameLoaded, onPlay } from './mill.js';
+import { onNewPlayer } from './index.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (gameLoaded) {
+    onPlay();
+  } else {
+    onNewPlayer();
+  }
+});
